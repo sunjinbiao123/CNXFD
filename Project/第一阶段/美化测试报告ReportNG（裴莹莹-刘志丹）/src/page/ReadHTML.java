98ab78f7e03678c606f8a9945937999982137907
刘志丹（8班） <279879153@qq.com>
2020-11-17 23:59:12 +0800

    Update ReadHTML.java

--- Project/第一阶段/美化测试报告ReportNG（裴莹莹-刘志丹）/src/page/ReadHTML.java
+++ Project/第一阶段/美化测试报告ReportNG（裴莹莹-刘志丹）/src/page/ReadHTML.java
@@ -6,13 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLEncoder;
 
-import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.HttpEntity;
-import org.apache.hc.core5.http.ParseException;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
 
 public class ReadHTML {
 	public ReadHTML() {
