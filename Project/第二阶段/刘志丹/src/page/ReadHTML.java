4ce486dd977d647208bf2b19a34061c78af36cb6
刘志丹（8班） <279879153@qq.com>
2020-11-28 14:19:46 +0800

    Add files via upload

---
+++ Project/第二阶段/刘志丹/src/page/ReadHTML.java
@@ -0,0 +1,30 @@
+package page;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+
+public class ReadHTML {
+	public ReadHTML() {
+		
+	}
+	public String readString(String url) throws ParseException, IOException {
+		File f = new File(url);
+		if(!f.exists() && !f.getAbsolutePath().endsWith("html")) {
+		return "";
+		} 
+		InputStream in = new FileInputStream(f) ;
+		StringBuffer b = new StringBuffer();
+		int c;
+		while((c =in.read()) != -1) {
+		b.append((char)c);
+		}
+		return b.toString();
+	}
+
+}
